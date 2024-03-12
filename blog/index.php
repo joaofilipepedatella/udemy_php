51c929521993b1bed3948906e9c88e2725549614
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-03-12 16:44:37 -0300

    Uso de composer e instalacao do SimpleRoute

--- blog/index.php
+++ blog/index.php
@@ -1,14 +1,7 @@
 <?php
 
 //Arquivo index responsável pela iniciação;
+require 'vendor/autoload.php';
+require 'rotas.php';
 
-require 'sistema/configuracao.php';
-include './sistema/Nucleo/Helpers.php';
-include './sistema/Nucleo/Mensagem.php';
-include './sistema/Nucleo/Controlador.php';
 
-use sistema\Nucleo\Controlador;
-
-$controlador = new Controlador();
-echo '<hr>';
-var_dump($controlador);
---
+++ blog/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'udemy_php/blog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5a2cf1e8511bff5b0882041befc641785ccb0f45',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pecee/simple-router' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b98d40b84b6e862bf250b179c0229efb7344a7bb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pecee/simple-router',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'udemy_php/blog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5a2cf1e8511bff5b0882041befc641785ccb0f45',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
