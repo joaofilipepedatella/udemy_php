11afd9548be99604c0821015d3cfc7a8650a26c0
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-03-14 18:58:23 -0300

    utilizacao da classe PostModelo para teste

--- blog/index.php
+++ blog/index.php
@@ -4,4 +4,13 @@
 require 'vendor/autoload.php';
 //require 'rotas.php';
 
+use sistema\Modelo\PostModelo;
 
+$posts = (new PostModelo())->ler(5);
+
+foreach($posts as $post){
+  echo $post->titulo.'<br>';
+}
+
+
+?>
