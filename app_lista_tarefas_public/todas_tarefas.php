a9efe4e6bdec62237ec8066170f74477d18fe513
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-04-04 14:46:26 -0300

    feat: add funcionalidade que determina a tarefa como completada.

--- app_lista_tarefas_public/todas_tarefas.php
+++ app_lista_tarefas_public/todas_tarefas.php
@@ -73,6 +73,12 @@ echo '</pre>'; */
 		function remover(id){
 			location.href = 'todas_tarefas.php?acao=remover&id='+id;
 		}
+
+		function marcarRealizada(id){
+			location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
+		}
+
+
 	</script>
 </head>
 
@@ -111,8 +117,15 @@ echo '</pre>'; */
 									<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
+
+									<?php 
+									if($tarefa->status == 'pendente') { ?>
+
 										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+										<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+
+									<?php } ?>
+									
 									</div>
 								</div>
 
